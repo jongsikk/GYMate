0d20c429b05fe9dd89a0a49caeaf7ae4ee9383ad
jongsikk <xxx_@kakao.com>
2025-01-13 23:26:06 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/jongsik2/training/gymate/GyMateApplication.java
@@ -0,0 +1,13 @@
+package com.jongsik2.training.gymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jongsik2/training/gymate/GyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongsik2.training.gymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

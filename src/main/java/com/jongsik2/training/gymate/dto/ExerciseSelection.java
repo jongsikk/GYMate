28652ab74d73e90eede707013086afa283d1b715
jongsikk <xxx_@kakao.com>
2025-01-23 01:20:39 +0900

    add: 운동 정보 DTO 추가

---
+++ src/main/java/com/jongsik2/training/gymate/dto/ExerciseSelection.java
@@ -0,0 +1,12 @@
+package com.jongsik2.training.gymate.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExerciseSelection {
+    private Long bodyType;
+    private Long muscleGroup;
+    private Long exercise;
+}

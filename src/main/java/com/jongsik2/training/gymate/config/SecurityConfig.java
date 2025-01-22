90b4a581ed39d44c2200f4e9e898766177c6e76d
jongsikk <xxx_@kakao.com>
2025-01-23 01:17:53 +0900

    add: Spring Security 설정

---
+++ src/main/java/com/jongsik2/training/gymate/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.jongsik2.training.gymate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) ->
+                        csrf.disable()
+                )
+                .headers((header) -> header.frameOptions(frameOptionsConfig ->
+                        frameOptionsConfig.disable())
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/login").permitAll()
+                                .requestMatchers("/api/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin((formLogin) ->
+                        formLogin
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/")
+                )
+        ;
+
+        return http.build();
+    }
+}

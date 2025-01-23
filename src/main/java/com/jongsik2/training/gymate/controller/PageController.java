a0af24e34e92c2b752fed83f7fffe42015b52a49
jongsikk <xxx_@kakao.com>
2025-01-23 23:01:17 +0900

    feat: 각 페이지 연결 컨트롤러 구현

--- src/main/java/com/jongsik2/training/gymate/controller/PageController.java
+++ src/main/java/com/jongsik2/training/gymate/controller/PageController.java
@@ -2,6 +2,8 @@ package com.jongsik2.training.gymate.controller;
 
 import com.jongsik2.training.gymate.dto.BodyTypeResponse;
 import com.jongsik2.training.gymate.dto.ExerciseSelection;
+import com.jongsik2.training.gymate.dto.ExerciseSetting;
+import com.jongsik2.training.gymate.dto.SignUpRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,8 +24,24 @@ import java.util.List;
 public class PageController {
     @GetMapping("/")
     public String index(Model model) {
-
         return "main";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String doSignup(@ModelAttribute SignUpRequest signUpRequest, RedirectAttributes redirectAttributes) {
+        log.info("doSignup: " + signUpRequest.getEmail() + " " + signUpRequest.getPassword() + " " + signUpRequest.getUsername());
+        redirectAttributes.addFlashAttribute("message", "회원가입이 완료되었습니다.");
+        return "redirect:/login";
     }
 
     @GetMapping("/exercise-selection")
@@ -33,7 +52,8 @@ public class PageController {
                 HttpMethod.GET,
                 new HttpEntity<>(null),
                 new ParameterizedTypeReference<List<BodyTypeResponse>>() {
-                });
+                }
+        );
 
         model.addAttribute("bodyTypes", response.getBody());
 
@@ -44,5 +64,12 @@ public class PageController {
     public String setSelection(@ModelAttribute ExerciseSelection exerciseSelection, Model model) {
         log.info(exerciseSelection.getExercise() + " " + exerciseSelection.getBodyType() + " " + exerciseSelection.getMuscleGroup());
         return "set-select";
+    }
+
+    @PostMapping("/start")
+    public String startExercise(@ModelAttribute ExerciseSetting exerciseSetting, Model model) {
+        log.info(exerciseSetting.getSets() + " " + exerciseSetting.getReps() + " " + exerciseSetting.getRestTime());
+        model.addAttribute("setting", exerciseSetting);
+        return "exercise";
     }
 }

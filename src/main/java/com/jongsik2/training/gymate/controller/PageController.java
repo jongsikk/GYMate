b1f45dfc7e3d6782614e60fc1649d82834f540ee
jongsikk <xxx_@kakao.com>
2025-02-09 22:37:10 +0900

    feat: JWT 로그인 구현

--- src/main/java/com/jongsik2/training/gymate/controller/PageController.java
+++ src/main/java/com/jongsik2/training/gymate/controller/PageController.java
@@ -1,27 +1,34 @@
 package com.jongsik2.training.gymate.controller;
 
-import com.jongsik2.training.gymate.dto.BodyTypeResponse;
 import com.jongsik2.training.gymate.dto.ExerciseSelection;
 import com.jongsik2.training.gymate.dto.ExerciseSetting;
+import com.jongsik2.training.gymate.dto.LoginRequest;
 import com.jongsik2.training.gymate.dto.SignUpRequest;
+import com.jongsik2.training.gymate.security.JwtUtil;
+import com.jongsik2.training.gymate.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class PageController {
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+
     @GetMapping("/")
     public String index(Model model) {
         return "main";
@@ -37,27 +44,30 @@ public class PageController {
         return "signup";
     }
 
-    @PostMapping("/signup")
-    public String doSignup(@ModelAttribute SignUpRequest signUpRequest, RedirectAttributes redirectAttributes) {
-        log.info("doSignup: " + signUpRequest.getEmail() + " " + signUpRequest.getPassword() + " " + signUpRequest.getUsername());
-        redirectAttributes.addFlashAttribute("message", "회원가입이 완료되었습니다.");
-        return "redirect:/login";
+    @PostMapping("/login")
+    public String doLogin(@ModelAttribute LoginRequest loginRequest, HttpServletResponse response, RedirectAttributes redirectAttributes) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+            );
+
+            addCookie(response, "access_token", jwtUtil.generateAccessToken(loginRequest.getEmail()), 60 * 30); // 30분 유지
+            addCookie(response, "refresh_token", jwtUtil.generateRefreshToken(loginRequest.getEmail()), 60 * 60 * 24); // 7일 유지
+
+            return "redirect:/";
+        } catch (BadCredentialsException e) {
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+
+            return "redirect:/login";
+        }
     }
 
-    @GetMapping("/exercise-selection")
-    public String exerciseSelection(Model model) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<BodyTypeResponse>> response = restTemplate.exchange(
-                "http://localhost:8080/api/body-type",
-                HttpMethod.GET,
-                new HttpEntity<>(null),
-                new ParameterizedTypeReference<List<BodyTypeResponse>>() {
-                }
-        );
+    @PostMapping("/signup")
+    public String doSignup(@ModelAttribute SignUpRequest signUpRequest, RedirectAttributes redirectAttributes) {
+        userService.register(signUpRequest);
+        redirectAttributes.addFlashAttribute("message", "회원가입이 완료되었습니다.");
 
-        model.addAttribute("bodyTypes", response.getBody());
-
-        return "exercise-select";
+        return "redirect:/login";
     }
 
     @PostMapping("/set-selection")
@@ -71,5 +81,14 @@ public class PageController {
         log.info(exerciseSetting.getSets() + " " + exerciseSetting.getReps() + " " + exerciseSetting.getRestTime());
         model.addAttribute("setting", exerciseSetting);
         return "exercise";
+    }
+
+    private void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
     }
 }
